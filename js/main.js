322ac46295f934e69171dc8f7c62887ef2b093d2
unknown <catalinapradilla@hotmail.com>
2018-02-21 10:48:27 +0100

    main.js + list functions called in html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+//    Global variables 
+var list = document.getElementById("todoList");
+var completeList = document.getElementById("completeList");
 
+
+// Create list Item from input value and create List
+function todoList() {   
+    var item = document.getElementById("taskInput").value;
+    var text = document.createTextNode(item);   
+    var newItem = document.createElement("li");
+
+ 
+  newItem.appendChild(text);   
+  list.appendChild(newItem); 
+
+
+} //function todolist () ends here.
+
+
+
+//    Reset List
+function resetList() {        
+    list.innerHTML = "";  
+
+}
+
+
+// Reset list on completedList
+function resetCompleteList() {        
+    completeList.innerHTML = "";
+
+}
 
 
