e6fcac5f6717a82026b7bbdf25e60a50627c6884
unknown <catalinapradilla@hotmail.com>
2018-02-21 21:05:33 +0100

    digital time function added + some changes in style and html

--- js/main.js
+++ js/main.js
@@ -11,10 +11,11 @@ function todoList() {
 
  	// Create Delete and Check buttons  
     var span = document.createElement("SPAN");
-    var textSpan = document.createTextNode("\u00D7");
+    var textSpan = document.createTextNode("\u232B");
     span.className = "close";
     var button = document.createElement("button");
-    var textButton = document.createTextNode("done");
+    var textButton = document.createTextNode("\u2713");
+    button.className = "done";
     
  
     // Remove list when click on delete button   
@@ -64,5 +65,55 @@ function resetCompleteList() {
     completeList.innerHTML = "";
 
 }
+
+//Current date format
+function renderTime () {
+
+	//Date variables
+	var mydate = new Date(); 
+	var year = mydate.getYear();
+		if(year < 1000){
+			year += 1900
+		}
+	var day = mydate.getDay();
+	var month = mydate.getMonth();
+	var daym = mydate.getDate();
+	var dayarray = new Array("Sunday,","Monday,","´Tuesday,","Wednesday,","Thursday,","Friday,","Saturday,");
+	var montharray = new Array("January","February","March","April","May","June","July","August","September", "October","November","December")
+	// Date End
+
+	//Time 
+	var currentTime = new Date ();
+	var h = currentTime.getHours();
+	var m = currentTime.getMinutes();
+	var s = currentTime.getSeconds();
+		if(h == 24){
+			h = 0;
+		} else if(h > 12){
+			h = h - 0;
+		}
+
+		if(h < 10){
+			h = "0" + h;
+		}
+
+		if(m < 10){
+			m = "0" + m;
+		}
+
+		if(s < 10){
+			s = "0" + s;
+		}
+
+		// end ifstatments with "0" to make it look digital
+
+		var myClock = document.getElementById("clockDisplay");
+		myClock.textContent = "" +dayarray[day]+ " " +daym+ " " +montharray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s;
+		myClock.innerText = "" +dayarray[day]+ " " +daym+ " " +montharray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s;
+
+		setTimeout("renderTime()", 1000);
+}
+renderTime();
+
 
 
