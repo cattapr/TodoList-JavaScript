14fa2cdad70dcd641fa19b2c04078f07f22497cb
unknown <catalinapradilla@hotmail.com>
2018-02-21 13:18:39 +0100

    done button + new functions

--- js/main.js
+++ js/main.js
@@ -13,7 +13,10 @@ function todoList() {
     var span = document.createElement("SPAN");
     var textSpan = document.createTextNode("\u00D7");
     span.className = "close";
+    var button = document.createElement("button");
+    var textButton = document.createTextNode("done");
     
+ 
     // Remove list when click on delete button   
     span.onclick = function() { 
       var div = this.parentElement;
@@ -21,9 +24,25 @@ function todoList() {
   }
 
 
+    // Check list when click on 'done button' + remove both elements when moved to completeList
+    button.onclick = function(){
+      const listItem = this.parentElement;
+      completeList.appendChild(listItem);
+      listItem.removeChild(button);
+
+    span.onclick = function() { 
+      var div = this.parentElement;
+      completeList.removeChild(div);
+    }
+
+  }
+
 	  // Appending to HTML
 	  newItem.appendChild(span);
 	  span.appendChild(textSpan); 
+
+	  newItem.appendChild(button);
+	  button.appendChild(textButton);
 
 	  newItem.appendChild(textItem);   
 	  list.appendChild(newItem); 
